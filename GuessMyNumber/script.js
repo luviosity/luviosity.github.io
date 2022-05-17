c205ea3fcd0658dbe2020ae3f98a18881a25e57d
Aleksandr <maestroj1995@mail.ru>
2022-05-18 01:13:13 +0300

    input with "enter" button

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -1,54 +1,63 @@
-'use strict';
+"use strict";
+
+const initialScore = 20;
+let playerNumber = 0;
+let score = Math.ceil(initialScore / 2);
+let highscore = 0;
 
 const displayMessage = (element, message) =>
   (document.querySelector(element).textContent = message);
 
-const initialScore = 20;
+// Return random number between 1 and initialScore
 const randomNumber = () => Math.trunc(Math.random() * initialScore) + 1;
 
 let guessNumber = randomNumber();
-let score = Math.ceil(initialScore / 2);
-console.log(score);
-let highscore = 0;
-let playerNumber = 0;
 
-document.querySelector('.check').addEventListener('click', function () {
-  playerNumber = Number(document.querySelector('.guess').value);
+const gameLogic = function () {
+  playerNumber = Number(document.querySelector(".guess").value);
 
   if (score !== 0) {
     if (!playerNumber) {
-      displayMessage('.message', '⛔ No number!');
+      displayMessage(".message", "⛔ No number!");
     } else if (playerNumber === guessNumber) {
-      displayMessage('.message', '🎉 You won!');
-      displayMessage('.number', guessNumber);
-      document.querySelector('body').style.backgroundColor = '#60b347';
-      document.querySelector('.number').style.width = '30rem';
+      displayMessage(".message", "🎉 You won!");
+      displayMessage(".number", guessNumber);
+      document.querySelector("body").style.backgroundColor = "#60b347";
+      document.querySelector(".number").style.width = "30rem";
       if (highscore < score) {
         highscore = score;
-        displayMessage('.highscore', highscore);
+        displayMessage(".highscore", highscore);
       }
     } else {
       score--;
-      displayMessage('.score', score);
+      displayMessage(".score", score);
       displayMessage(
-        '.message',
+        ".message",
         score === 0
-          ? '😭 You lost.'
+          ? "😭 You lost."
           : playerNumber > guessNumber
-          ? '📈 Too high.'
-          : '📉 Too low.'
+          ? "📈 Too high."
+          : "📉 Too low."
       );
     }
   }
+};
+
+// Gaming process via "Check!" button
+document.querySelector(".check").addEventListener("click", gameLogic);
+
+// Reset game via "Again!" button
+document.querySelector(".again").addEventListener("click", function () {
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector(".guess").value = "";
+  displayMessage(".number", "?");
+  displayMessage(".message", "Start guessing...");
+  score = Math.ceil(initialScore / 2);
+  displayMessage(".score", score);
+  guessNumber = randomNumber();
 });
 
-document.querySelector('.again').addEventListener('click', function () {
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
-  document.querySelector('.guess').value = '';
-  displayMessage('.number', '?');
-  displayMessage('.message', 'Start guessing...');
-  score = Math.ceil(initialScore / 2);
-  displayMessage('.score', score);
-  guessNumber = randomNumber();
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") gameLogic();
 });
