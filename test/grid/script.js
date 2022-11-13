7a0c5649ce045c64facdce3265f4cdbfe8047a2b
Aleksandr <53473874+luviosity@users.noreply.github.com>
2022-11-13 12:28:15 +0300

    Delete test directory

--- test/grid/script.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector('.more__btn');
-const moreContent = document.querySelector('.more__content');
-
-btn.addEventListener('click', function () {
-  const style = moreContent.style;
-  if (style.display) style.display = '';
-  else style.display = 'block';
-});
